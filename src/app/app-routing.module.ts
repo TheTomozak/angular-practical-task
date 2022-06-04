c1e5655d59b099a9a8ee0dd650f6121c62158994
Tomozak <s18969@pjwstk.edu.pl>
2022-06-04 16:01:26 +0200

    feat: create lazy-load search bar module without logic, add bootstrap 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'search-bar', loadChildren: () => import('./search-bar/search-bar.module').then(m => m.SearchBarModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
